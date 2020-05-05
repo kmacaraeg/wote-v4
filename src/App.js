f606535515fc47cd2d4f62f48123723388e020ef
Kevin Macaraeg <kmacaraeg@theorchard.com>
2020-05-05 01:31:39 -0400

    welcome back kevin

--- src/App.js
+++ src/App.js
@@ -31,14 +31,37 @@ const Display = styled.div`
   width: 100vw;
   background: url(img/background.jpg);
   background-size: cover;
+  display: flex;
+
+  @media (max-width: 1000px) {
+    display: none;
+  }
+
+`
+
+const MobileDisplay = styled.div`
+
+display: none;
+@media (min-width: 1000px) {
+  display: flex;
+}
+
+height: 100vh;
+width: 100vw;
+background: url(img/background.jpg);
+background-size: cover;
+display: flex;
 
 `
 
 const Welcome = styled.div`
 
+  display: flex;
+
   .welcome-container {
+    display: flex;
     background-color: white;
-    height: 300px;
+    // height: 300px;
     // width: 660px;
     padding: 50px;
     flex-direction: column;
@@ -48,6 +71,7 @@ const Welcome = styled.div`
   }
 
   .welcome-logo {
+    display: flex;
     background: url(img/logo.png);
     height: 100px;
     width: 300px;
@@ -90,8 +114,9 @@ const Welcome = styled.div`
 const Merch = styled.div`
 
   .merch-container {
+    display: flex;
     background-color: white;
-    height: 500px;
+    // height: 500px;
     // width: 500px;
     padding: 50px;
     flex-direction: column;
@@ -101,10 +126,12 @@ const Merch = styled.div`
   }
 
   .merch-thumbnails-container {
-    width: 660px;
+    display: flex;
+    width: 700px;
     flex-wrap: wrap;
     max-height: 500px;
     overflow: scroll;
+    overflow-x: hidden;
   }
 
   .merch-thumbnail {
@@ -117,8 +144,9 @@ const Merch = styled.div`
   }
   
   .merch-thumbnail-picture {
-    background-color: lightgray;
-    height: 300px;
+    // background-color: lightgray;
+    background-size: contain;
+    height: 200px;
     width: 200px;
     margin-bottom: 10px;
   }
@@ -144,11 +172,16 @@ const Merch = styled.div`
     margin-top: 15px;
     margin-bottom: 5px;
   }
+
+  img {
+    height: 200px;
+    width: 200px;
+  }
 `
 
 const MainGame = styled.div`
 
-
+  
 
 `
 
@@ -163,6 +196,10 @@ const ModalButton = styled.div`
   // color: ${props => props.theme.pink};
   color: white;
   font-weight: bold;
+  @media (max-width: 1000px) {
+    display: none;
+  }
+
 
 `
 
@@ -176,10 +213,21 @@ const Modal = styled.div`
   overflow: scroll;
   display: block;
 
+  @media (min-width: 1000px) {
+height: 100%;
+width: 100%;
+  }
+
   .modal-container {
     flex-direction: column;
     margin-top: 100px;
     margin-bottom: 100px;
+    display: flex;
+
+    @media (min-width: 1000px) {
+      margin-top: 100px;
+      margin-bottom: 100px;
+    }
   }
 
   .modal-list-container {
@@ -190,6 +238,15 @@ const Modal = styled.div`
     padding: 50px;
     border-radius: 10px;
     z-index: 50;
+    border-radius: 10px;
+    border: 2px solid ${props => props.theme.pink};
+
+    @media (max-width: 1000px) {
+      padding: 0px;
+      width: 100%;
+      // display: none;
+    }
+    
   }
 
   .modal-list-item {
@@ -198,6 +255,10 @@ const Modal = styled.div`
     border-bottom: 1px solid black;
     padding-bottom: 10px;
     margin-bottom: 10px;
+    @media (max-width: 1000px) {
+      width: 100%;
+      margin: 20px;
+    }
   }
 
   .modal-list-item:hover {
@@ -210,6 +271,8 @@ const Modal = styled.div`
     border-radius: 10px;
     border: none;
     z-index: 50;
+    border-radius: 10px;
+    border: 2px solid ${props => props.theme.pink};
   }
 
   h1 {
@@ -223,23 +286,49 @@ const Modal = styled.div`
       margin-bottom: 0;
   }
 
+  .freebirdFormviewerViewItemsItemItem {
+    border-radius: 10px;
+    border: 2px solid ${props => props.theme.pink};
+  }
+
 `
 
 const GameRender = styled.div`
 
+  display: flex;
   flex-direction: column;
 
   .game-chooser {
+    display: flex;
     flex-wrap: wrap;
     width: 500px;
     box-shadow: 0px 0px 20px rgba(0,0,0,.4);
     z-index: 50;
+    border-radius: 10px;
+    border: 2px solid ${props => props.theme.pink};
   }
 
   .game-choice {
+    display: flex;
     width: 50%;
     height: 200px;
     background-color: white;
+    // border-radius: 10px;
+
+
+  }
+
+  .tl {
+    border-radius: 10px 0px 0px 0px;
+  }
+  .tr {
+    border-radius: 0px 10px 0px 0px;
+  }
+  .br {
+    border-radius: 0px 0px 10px 0px;
+  }
+  .bl {
+    border-radius: 0px 0px 0px 10px;
   }
 
   .game-choice:hover {
@@ -259,6 +348,7 @@ const GameRender = styled.div`
   }
 
   .game-instructions {
+    display: flex;
     height: 40px;
     background-color:${props => props.theme.pink};
     color: white;
@@ -276,23 +366,26 @@ const GameRender = styled.div`
   }
 
   .game-crossword {
+    flex-direction: row;
+    display: block;
     width: 1000px;
-    background-color: white;
-    height: 500px;
-    box-shadow: 0px 0px 20px rgba(0,0,0,.4);
-    z-index: 50;
-    color: black;
-    font-style: italic;
-    font-weight: bold;
-    letter-spacing: 2px;
+    height: auto;
+    div {
+      display: block
+    }
   }
 
   .game-puzzle {
     width: 700px;
     background-color: white;
-    height: 400px;
+    // height: 400px;
+    padding: 50px;
     box-shadow: 0px 0px 20px rgba(0,0,0,.4);
     z-index: 50;
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+    border: 2px solid ${props => props.theme.pink};
   }
 
   .game-search {
@@ -314,7 +407,7 @@ const GameRender = styled.div`
   .game-puzzle-text {
     display: flex;
     flex-direction: column;
-    padding: 40px;
+    margin-bottom: 40px;
   }
 `
 
@@ -334,47 +427,128 @@ const CornerLogo = styled.div`
   &:hover {
     opacity: .8;
   }
+  @media (max-width: 1000px) {
+    display: none;
+  }
+
 
 `
 
 const merchArray = [
   {
-    name: "jacket",
-    picture: "red",
-    description: "info",
+    picture: "img/merch_1.png",
     link: "/"
   },
   {
-    name: "shirt",
-    picture: "blue",
-    description: "info",
+    picture: "img/merch_2.png",
     link: "/"
   },
   {
-    name: "hat",
-    picture: "red",
-    description: "info",
+    picture: "img/merch_3.png",
     link: "/"
   },
   {
-    name: "cd",
-    picture: "blue",
-    description: "info",
+    picture: "img/merch_4.png",
     link: "/"
   },
   {
-    name: "socks",
-    picture: "red",
-    description: "info",
+    picture: "img/merch_5.png",
     link: "/"
   },
   {
-    name: "watch",
-    picture: "blue",
-    description: "info",
+    picture: "img/merch_6.png",
     link: "/"
-  }
+  },
+  {
+    picture: "img/merch_7.png",
+    link: "/"
+  },
 ]
+
+const crosswordData2 = {
+  id: 'simple/1',
+  number: 1,
+  name: 'Simple Crossword 1',
+  date: 1542326400000,
+  entries: [
+    {
+      id: '1-across',
+      number: 1,
+      humanNumber: '1',
+      clue: 'Toy on a string (2-2)',
+      direction: 'across',
+      length: 4,
+      group: ['1-across'],
+      position: { x: 0, y: 0 },
+      separatorLocations: {
+        '-': [2],
+      },
+      solution: 'YOYO',
+    },
+    {
+      id: '2-across',
+      number: 2,
+      humanNumber: '2',
+      clue: 'Have a rest (3,4)',
+      direction: 'across',
+      length: 7,
+      group: ['2-across'],
+      position: { x: 0, y: 2 },
+      separatorLocations: {
+        ',': [3],
+      },
+      solution: 'LIEDOWN',
+    },
+    {
+      id: '1-down',
+      number: 1,
+      humanNumber: '1',
+      clue: 'Colour (6)',
+      direction: 'down',
+      length: 6,
+      group: ['1-down'],
+      position: { x: 0, y: 0 },
+      separatorLocations: {},
+      solution: 'YELLOW',
+    },
+    {
+      id: '3-down',
+      number: 3,
+      humanNumber: '3',
+      clue: 'Bits and bobs (4,3,4)',
+      direction: 'down',
+      length: 7,
+      group: ['3-down', '4-down'],
+      position: { x: 3, y: 0 },
+      separatorLocations: {
+        ',': [4],
+      },
+      solution: 'ODDSAND',
+    },
+    {
+      id: '4-down',
+      number: 4,
+      humanNumber: '4',
+      clue: 'See 3 down',
+      direction: 'down',
+      length: 4,
+      group: ['3-down', '4-down'],
+      position: {
+        x: 6,
+        y: 1,
+      },
+      separatorLocations: {},
+      solution: 'ENDS',
+    },
+  ],
+  solutionAvailable: true,
+  dateSolutionAvailable: 1542326400000,
+  dimensions: {
+    cols: 13,
+    rows: 13,
+  },
+  crosswordType: 'quick',
+}
 
 
 class App extends Component {
@@ -385,6 +559,7 @@ class App extends Component {
     // 0 = Welcome
     // 1 = Merch
     // 2 = Main game
+    // 50 = crossword test
 
     theme: {
       brown: "#583539",
@@ -453,7 +628,7 @@ class App extends Component {
           }}>
             {tourStops.map((x, i) => (
               <div className="modal-list-item">
-                                  <h3>{x.date}</h3>
+                <h3>{x.date}</h3>
                 <a href="https://www.bandsintown.com/a/158776-walk-off-the-earth?came_from=242" >
 
                   <h3>{x.city}, {x.state}</h3>
@@ -463,11 +638,11 @@ class App extends Component {
           </div>
         </div>
         <div className="modal-container">
-        <h1>Music</h1>
-        <Iframe className="iframe modal-spotify" url="https://open.spotify.com/embed/album/02TmceYFis4XKcjCZ86eBP" width="500px" height="400px" border="none"></Iframe>
+          <h1>Music</h1>
+          <Iframe className="iframe modal-spotify" url="https://open.spotify.com/embed/album/02TmceYFis4XKcjCZ86eBP" width="500px" height="400px" border="none"></Iframe>
         </div>
         <div className="modal-container">
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdczIih8EqO1aHjaMqnLk-u-tkMMg3kbrJ0Lloavz4Ae4YQFQ/viewform?embedded=true" width="640" height="1171" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdczIih8EqO1aHjaMqnLk-u-tkMMg3kbrJ0Lloavz4Ae4YQFQ/viewform?embedded=true" width="640" height="1171" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </div>
       </Modal>
     }
@@ -478,26 +653,26 @@ class App extends Component {
     switch (this.state.currentGame) {
       case "home":
         return <div className="game-chooser">
-          <div className="game-choice" onClick={() => { this.setState({ currentGame: "crossword" }) }}>Crossword</div>
-          <div className="game-choice" onClick={() => { this.setState({ currentGame: "puzzle" }) }}>Puzzle</div>
-          <div className="game-choice" onClick={() => { this.setState({ currentGame: "search" }) }}>Search</div>
-          <div className="game-choice" onClick={() => { this.setState({ currentGame: "draw" }) }}>Draw</div>
+          <div className="game-choice tl" onClick={() => { this.setState({ currentGame: "crossword" }) }}>Crossword</div>
+          <div className="game-choice tr" onClick={() => { this.setState({ currentGame: "puzzle" }) }}>Puzzle</div>
+          <div className="game-choice bl" onClick={() => { this.setState({ currentGame: "search" }) }}>Search</div>
+          <div className="game-choice br" onClick={() => { this.setState({ currentGame: "draw" }) }}>Draw</div>
         </div>
       case "crossword":
         return <div className="game-crossword"><Crossword data={crosswordData}></Crossword></div>
-        case "puzzle":
-            return <div className="game-puzzle">
-            <div className="game-puzzle-text">
-              <h1>Solve the Puzzle!</h1>
-              <p>What is going on? Click to move the tiles and solve the puzzle!</p>
-            </div>
-            <div className="sliderParts">
+      case "puzzle":
+        return <div className="game-puzzle">
+          <div className="game-puzzle-text">
+            <h1>Solve the Puzzle!</h1>
+            <p>What is going on? Click to move the tiles and solve the puzzle!</p>
+          </div>
+          <div className="sliderParts">
             <SlidingTilePuzzle
-          maxIterations={300}
-          image={"img/Waldo_OG.jpg"}
-          size={400}
-        />
-            </div>
+              maxIterations={300}
+              image={"img/Waldo_OG.jpg"}
+              size={400}
+            />
+          </div>
         </div>
       // case "search":
       //   return  <CompareImgInput className="compareImageInput" data={"test"} rightImage={"img/waldo_1"} />
@@ -509,8 +684,8 @@ class App extends Component {
   }
 
   renderToolbar() {
-    if(this.state.currentGame!=="home"){
-      return <div className="game-chooser-toolbar" onClick={() => { this.setState({ currentGame: "home" })}}>back home</div>
+    if (this.state.currentGame !== "home") {
+      return <div className="game-chooser-toolbar" onClick={() => { this.setState({ currentGame: "home" }) }}>back home</div>
     }
   }
 
@@ -523,7 +698,7 @@ class App extends Component {
             <div className="welcome-logo"></div>
             <div className="welcome-copy">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis laoreet metus, ut tristique dolor. Vivamus eu ante ut velit lacinia molestie. Fusce nisl dolor, mattis a pretium et, aliquet sit amet leo.
+                Hey there! This is a personal message from the band. Thanks for checking out our new game site. Here we go!
                 </p>
             </div>
             <div className="welcome-enterbutton" onClick={() => { this.setState({ currentWindow: 1 }) }}>
@@ -538,10 +713,10 @@ class App extends Component {
             <div className="merch-thumbnails-container">
               {/* map function for merch items */}
               {merchArray && merchArray.map((x, i) => (
-                <div className="merch-thumbnail" onClick={() => { }}>
-                  <div className="merch-thumbnail-picture"></div>
-                  <h3>{x.name}</h3>
-                  <h4>{x.description}</h4>
+                <div className="merch-thumbnail" style={{}} onClick={() => { }}>
+                  {/* <div className="merch-thumbnail-picture"></div> */}
+                  <img src={x.picture}></img>
+                  {/* <h3>{x.name}</h3> */}
                 </div>
               ))
               }
@@ -555,34 +730,87 @@ class App extends Component {
       case 2:
         return <MainGame theme={this.state.theme}>
           <GameRender theme={this.state.theme}>
-            <div className="game-instructions">Here we go!</div>
-            {this.renderGame()}
-            {this.renderToolbar()}
-          </GameRender>
+            <div className="game-puzzle">
+              <div className="game-puzzle-text">
+                <h1>Solve the Puzzle!</h1>
+                <p>What's going on? Click to move the tiles and solve the puzzle!</p>
+              </div>
+              <div className="sliderParts">
+                <SlidingTilePuzzle
+                  maxIterations={300}
+                  image={"img/Waldo_OG.jpg"}
+                  size={400}
+                />
+              </div>
+              {this.renderToolbar()}
+              </div>
+              </GameRender>
 
         </MainGame>
-    }
-  }
 
+          case 3:
+                  return <MainGame theme={this.state.theme}>
+            <GameRender theme={this.state.theme}>
+              <div className="game-instructions">Here we go!</div>
+              {this.renderGame()}
+              {this.renderToolbar()}
+            </GameRender>
+
+          </MainGame>
+          case 50:
+          return <MainGame theme={this.state.theme}>
+            <GameRender theme={this.state.theme}>
+              <Crossword style={{ display: "block" }} className="game-crossword" data={crosswordData2} />
+            </GameRender>
+          </MainGame>
+          }
+        }
+      
   render() {
     return (
       <Router>
-        <ReactTooltip data-border={false} />
-        <CornerLogo onClick={()=>{this.setState({currentWindow: 1})}}></CornerLogo>
-        <ModalButton  theme={this.state.theme} onClick={() => {
-          this.setState({ modalShown: !this.state.modalShown })
-        }}>MENU</ModalButton>
-        {this.checkModal()}
-        <Switch>
-          <Route path="/">
-            <Display theme={this.state.theme} currentWindow={this.state.currentWindow}>
-              {this.readCurrentWindow(this.state.currentWindow)}
-            </Display>
-          </Route>
-        </Switch>
-      </Router>
-    )
-  }
-}
+            <ReactTooltip data-border={false} />
+            <CornerLogo data-tip="Merch Store" onClick={() => { this.setState({ currentWindow: 1 }) }}></CornerLogo>
+            <ModalButton theme={this.state.theme} onClick={() => {
+              this.setState({ modalShown: !this.state.modalShown })
+            }}>MENU</ModalButton>
+            {this.checkModal()}
+            <Switch>
+              <Route path="/">
+                <Display theme={this.state.theme} currentWindow={this.state.currentWindow}>
+                  {this.readCurrentWindow(this.state.currentWindow)}
+                </Display>
+                <MobileDisplay>
+                <Modal theme={this.state.theme}>
+        <div className="modal-container">
+          <h1>Tour Dates</h1>
+          <div className="modal-list-container" onClick={() => {
+          }}>
+            {tourStops.map((x, i) => (
+              <div className="modal-list-item">
+                <h3>{x.date}</h3>
+                <a href="https://www.bandsintown.com/a/158776-walk-off-the-earth?came_from=242" >
 
-export default App;
+                  <h3>{x.city}, {x.state}</h3>
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="modal-container">
+          <h1>Music</h1>
+          <Iframe className="iframe modal-spotify" url="https://open.spotify.com/embed/album/02TmceYFis4XKcjCZ86eBP" width="500px" height="400px" border="none"></Iframe>
+        </div>
+        <div className="modal-container">
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdczIih8EqO1aHjaMqnLk-u-tkMMg3kbrJ0Lloavz4Ae4YQFQ/viewform?embedded=true" width="640" height="1171" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        </div>
+      </Modal>
+                </MobileDisplay>
+              </Route>
+            </Switch>
+          </Router>
+          )
+        }
+      }
+      
+      export default App;
